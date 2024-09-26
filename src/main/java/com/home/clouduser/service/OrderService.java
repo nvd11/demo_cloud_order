a7cb396ce025c49bdfcbd66566ef686e23bba1aa
nvd11 <nvd11@163.com>
2024-09-27 01:30:10 +0800

    some testing codes

--- src/main/java/com/home/clouduser/service/OrderService.java
+++ src/main/java/com/home/clouduser/service/OrderService.java
@@ -3,13 +3,14 @@ package com.home.clouduser.service;
 
 import com.home.clouduser.dao.OrderDao;
 import com.home.clouduser.entities.Order;
+import jakarta.persistence.QueryTimeoutException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import java.util.NoSuchElementException;
-import java.util.Optional;
 
 @Service
+@Slf4j
 public class OrderService {
 
     @Autowired
@@ -18,17 +19,30 @@ public class OrderService {
     // this method is used to query order by id
     public Order queryById(Long id) {
         // if optional.isPresent() is false, it will throw NoSuchElementException
+        return orderDao.findById(id).orElseThrow(() -> new NoSuchElementException("No such order by id: " + id));
+        /*
         Optional<Order> optional = orderDao.findById(id);
         if (optional.isPresent()) {
             return optional.get();
         }else {
             throw new NoSuchElementException("No such order by id: " + id);
         }
+
+        */
     }
 
     public Order updateOrder(Long orderId, Order orderDetails) {
 
-        Order order = orderDao.findById(orderId).orElseThrow(() -> new NoSuchElementException("No such order by id: " + orderId));
+        Order order = null;
+        try{
+            order = this.queryById(orderId);
+        } catch (NoSuchElementException e) {
+            log.error("Error in getting order by id...", e);
+            throw e;
+        } catch (QueryTimeoutException e) {
+            log.error("timeout..", e);
+            return null;
+        }
 
         order.setCommodityName(orderDetails.getCommodityName());
         order.setPrice(orderDetails.getPrice());
--- src/test/java/com/home/clouduser/service/OrderServiceBddTest.java
+++ src/test/java/com/home/clouduser/service/OrderServiceBddTest.java
@@ -3,28 +3,36 @@ package com.home.clouduser.service;
 import com.home.clouduser.configs.DebugLoggerExtension;
 import com.home.clouduser.dao.OrderDao;
 import com.home.clouduser.entities.Order;
+import jakarta.persistence.QueryTimeoutException;
 import lombok.extern.slf4j.Slf4j;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.Mockito;
+
 import org.springframework.test.util.ReflectionTestUtils;
 
+import java.sql.SQLTimeoutException;
 import java.util.NoSuchElementException;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 @Slf4j
-@ExtendWith(DebugLoggerExtension.class)
-class OrderServiceTest {
 
-    //@Mock
-    //private OrderDao orderDao;
-//
-    //@InjectMocks
-    //private OrderService orderService;
+@ExtendWith(MockitoExtension.class)
+class OrderServiceBddTest {
+
+    @Mock
+    private OrderDao orderDao;
+
+    @InjectMocks
+    private OrderService orderService;
 
     private Order orderToTest;
 
@@ -39,44 +47,39 @@ class OrderServiceTest {
     @Test
     void updateOrder() {
         log.info("updateOrder test start");
-        OrderDao orderDao = Mockito.mock(OrderDao.class);
-        OrderService orderService = new OrderService();
-        ReflectionTestUtils.setField(orderService, "orderDao", orderDao); // set the injected field
+
 
         // ==== happy flow ==========================================================================
         // force the orderDao to return the orderToTest
-        Mockito.when(orderDao.findById(Mockito.anyLong()))
-                .thenReturn(Optional.of(orderToTest));
+        given(orderDao.findById(Mockito.anyLong())).willReturn(Optional.of(orderToTest));
 
-        /*
-         *  invocation is a concept of the mockito invocation, it includes the context information of the mock
-         *  e.g. the argument of the method,  return values
-         *
-         *  usually, you can use the argument(0) to get the first argument
-         *
-         *  some methods
-         *  getArgument(int index): get the argument at index
-         *  getArguments(): get all arguments
-         *  getMethod(): get the method
-         *  getMock(): get the mock object
-         */
-
-        // means let orderDao save order return the argument it receives
-        Mockito.when(orderDao.save(Mockito.any(Order.class))).thenAnswer(invocation -> invocation.getArgument(0));
+        // let orderDao.save() successfully return the orderToTest
+        given(orderDao.save(any(Order.class))).willAnswer(invocation -> invocation.getArgument(0));
 
 
         Order orderUpdated = orderService.updateOrder(101L, orderDetails);
+        Mockito.verify(orderDao).findById(101L);
+        //Mockito.verify(orderService).queryById(101L); // not work
         assertEquals(orderDetails.getCommodityName(), orderUpdated.getCommodityName());
         assertEquals(orderDetails.getPrice(), orderUpdated.getPrice());
 
 
         // ==== exception case 1 , order not found ============================================================================
-        Mockito.when(orderDao.findById(Mockito.anyLong()))
-                .thenThrow(NoSuchElementException.class);
+
+        // clean all stubs for an object
+        Mockito.reset(orderDao);
+
+        given(orderDao.findById(Mockito.anyLong())).willThrow(NoSuchElementException.class);
 
         assertThrows(NoSuchElementException.class, () -> orderService.updateOrder(101L, orderDetails));
 
-        //
+        // ==== exception case 1 , order not found ============================================================================
+        Mockito.reset(orderDao);
+        given(orderDao.findById(Mockito.anyLong())).willThrow(QueryTimeoutException.class);
+
+        assertDoesNotThrow(() -> orderService.updateOrder(101L, orderDetails));
+        Mockito.verify(orderDao).findById(101L);
+        assertNull(orderService.updateOrder(101L, orderDetails));
     }
 
 
